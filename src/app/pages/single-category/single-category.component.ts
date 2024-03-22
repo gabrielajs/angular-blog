687611bf50ab34fd971fd3738cbfb4cf0eb0986c
Gabi <gabrielajustinodesouza@gmail.com>
2024-03-22 00:23:14 -0300

    criando componente pages/single-category

---
+++ src/app/pages/single-category/single-category.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-category',
+  standalone: true,
+  imports: [],
+  templateUrl: './single-category.component.html',
+  styleUrl: './single-category.component.sass'
+})
+export class SingleCategoryComponent {
+
+}
