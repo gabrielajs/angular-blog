bd441926d17674ddf4d42815000e0169e409ae43
Gabi <gabrielajustinodesouza@gmail.com>
2024-03-22 00:55:10 -0300

    configurando rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SingleCategoryComponent } from './pages/single-category/single-category.component';
+import { SinglePostComponent } from './pages/single-post/single-post.component';
+import { TermsAndConditionComponent } from './pages/terms-and-condition/terms-and-condition.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'category',
+        component: SingleCategoryComponent
+    },
+    {
+        path: 'post',
+        component: SinglePostComponent
+    },
+    {
+        path: 'term-condition',
+        component: TermsAndConditionComponent
+    },
+    {
+        path: 'contatus',
+        component: ContactUsComponent
+    }
+];
