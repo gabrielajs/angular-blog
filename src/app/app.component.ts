6190ce5e14cb1619b4401de7b53290b844b50932
Gabi <gabrielajustinodesouza@gmail.com>
2024-03-24 22:10:14 -0300

    criando layout do menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./layouts/header/header.component";
+import { CategoryNavbarComponent } from "./layouts/category-navbar/category-navbar.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.sass',
+    imports: [RouterOutlet, HeaderComponent, CategoryNavbarComponent]
 })
 export class AppComponent {
   title = 'angular-blog';
