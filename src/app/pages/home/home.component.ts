fe6f4465497adc75127bdb458019133e08ba4dfd
Gabi <gabrielajustinodesouza@gmail.com>
2024-03-28 14:35:04 -0300

    criando formulario subscribe

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from "../../layouts/footer/footer.component";
 import { PostCardComponent } from "../../layouts/post-card/post-card.component";
+import { SubscriptionFormComponent } from "../../subscription-form/subscription-form.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.sass',
-    imports: [FooterComponent, PostCardComponent]
+    imports: [FooterComponent, PostCardComponent, SubscriptionFormComponent]
 })
 export class HomeComponent {
 
